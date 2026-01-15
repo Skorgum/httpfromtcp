c45f269df81b153d784b610da02041d075e12354
Skorgum <skorgum@gmail.com>
2026-01-14 23:44:39 -0500

    Add handler

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/Skorgum/httpfromtcp/internal/request"
+	"github.com/Skorgum/httpfromtcp/internal/response"
 	"github.com/Skorgum/httpfromtcp/internal/server"
 )
 
 const port = 42069
 
 func main() {
-	srv, err := server.Serve(port)
+	handler := func(w io.Writer, req *request.Request) *server.HandlerError {
+		switch req.RequestLine.RequestTarget {
+		case "/yourproblem":
+			return &server.HandlerError{
+				StatusCode: response.StatusBadRequest,
+				Message:    "Your problem is not my problem\n",
+			}
+		case "/myproblem":
+			return &server.HandlerError{
+				StatusCode: response.StatusInternalServerError,
+				Message:    "Woopsie, my bad\n",
+			}
+		default:
+			w.Write([]byte("All good, frfr\n"))
+			return nil
+		}
+	}
+
+	srv, err := server.Serve(port, handler)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,19 +1,38 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net"
 	"sync/atomic"
 
+	"github.com/Skorgum/httpfromtcp/internal/request"
 	"github.com/Skorgum/httpfromtcp/internal/response"
 )
 
+type Handler func(w io.Writer, req *request.Request) *HandlerError
+
 type Server struct {
-	ln     net.Listener
-	closed atomic.Bool
+	ln      net.Listener
+	handler Handler
+	closed  atomic.Bool
 }
 
-func Serve(port int) (*Server, error) {
+type HandlerError struct {
+	StatusCode response.StatusCode
+	Message    string
+}
+
+func (he HandlerError) Write(w io.Writer) {
+	body := []byte(he.Message)
+	response.WriteStatusLine(w, he.StatusCode)
+	headers := response.GetDefaultHeaders(len(body))
+	response.WriteHeaders(w, headers)
+	w.Write(body)
+}
+
+func Serve(port int, handler Handler) (*Server, error) {
 	addr := fmt.Sprintf(":%d", port)
 	ln, err := net.Listen("tcp", addr)
 	if err != nil {
@@ -21,7 +40,8 @@ func Serve(port int) (*Server, error) {
 	}
 
 	srv := &Server{
-		ln: ln,
+		ln:      ln,
+		handler: handler,
 	}
 
 	go srv.listen()
@@ -50,13 +70,27 @@ func (s *Server) listen() {
 func (s *Server) handle(conn net.Conn) {
 	defer conn.Close()
 
-	if err := response.WriteStatusLine(conn, response.StatusOk); err != nil {
+	req, err := request.RequestFromReader(conn)
+	if err != nil {
+		hErr := HandlerError{
+			StatusCode: response.StatusBadRequest,
+			Message:    err.Error(),
+		}
+		hErr.Write(conn)
 		return
 	}
 
-	h := response.GetDefaultHeaders(0)
+	buf := bytes.NewBuffer(nil)
 
-	if err := response.WriteHeaders(conn, h); err != nil {
+	hErr := s.handler(buf, req)
+	if hErr != nil {
+		hErr.Write(conn)
 		return
 	}
+
+	body := buf.Bytes()
+	response.WriteStatusLine(conn, response.StatusOk)
+	headers := response.GetDefaultHeaders(len(body))
+	response.WriteHeaders(conn, headers)
+	conn.Write(body)
 }
